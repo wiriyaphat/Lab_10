e51a5805acf5b852407384f24fd1db5c60047ad3
Thiw <wiriyaphat6789@gmail.com>
2025-12-23 15:26:56 +0700

    Lab10_2 complete

--- lab10_2.cpp
+++ lab10_2.cpp
@@ -6,13 +6,17 @@ using namespace std;
 int main (){
 	ifstream source;
 	ofstream dest;
-	source.open("cheerbook.txt") ;
+	source.open("cheerbook.txt");
 	dest.open("cheerbook_copy.txt");
-	
+	string txt;
+	dest << "-------------------- BOOM ---------------------\n";
+	while(getline(source, txt)){
+		dest << txt << "\n";
+	}
+	dest << "-------------------- HA!! ---------------------\n";
 	//กรอก Code เฉพาะส่วนนี้ ให้สอดคล้องกับโจทย์และ Code ส่วนอื่น ๆ 
 	//โดยห้ามแก้ไข Code ในส่วนอื่น ๆ
-
-    source.close();
+	source.close();
     dest.close();
-	return 0;
+	return 0; 
 }
