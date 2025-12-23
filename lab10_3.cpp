3aca22b16084a8495dd050f0963ff30416bbd178
Thiw <wiriyaphat6789@gmail.com>
2025-12-23 15:47:45 +0700

    Lab10_3 complete

--- lab10_3.cpp
+++ lab10_3.cpp
@@ -1,3 +1,29 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+    #include <iostream>
+    #include <fstream>
+    #include <string>
+    #include <cmath>
+    #include <iomanip>
+    using namespace std;
+
+    int main() {
+        string txt;
+        ifstream Summ;
+        Summ.open("score.txt");
+        double sigma = 0;
+        int count = 0;
+        double sum = 0;
+        double sumJ = 0;
+        while(getline (Summ,txt)){
+            sum += atof(txt.c_str());
+            sumJ += pow(atof(txt.c_str()),2);
+            count ++;
+        }
+        double Mean = sum/count;
+        sigma += sqrt((sumJ/count) - pow(Mean,2));
+        cout << "Number of data = " << count << "\n";
+        cout << setprecision(3);
+        cout << "Mean = " << Mean << "\n";
+        cout << "Standard deviation = " << sigma << "\n";
+        Summ.close();
+        return 0;
+    }
